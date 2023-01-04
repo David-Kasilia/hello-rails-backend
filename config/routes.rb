58386dd054f2b974a9df9f13be9c92b6d65b2c42
David-Kasilia <davidkasilia001@gmail.com>
2023-01-04 23:27:16 +0300

    Completed project api setup successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'message/index'
+
+  resources :messages
+
+  # root "root#index"
+  # get 'message/show'
+  # get 'message/create'
+  # get 'message/new'
+  # get 'message/destroy'
+  # get 'message/delete'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ test/controllers/message_controller_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class MessageControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get message_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get message_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get message_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get message_new_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get message_destroy_url
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get message_delete_url
+    assert_response :success
+  end
+end
